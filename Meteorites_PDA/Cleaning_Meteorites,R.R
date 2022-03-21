3ddfad3b73468d3ccfeb48ccfeb6dfd87fbcb72f
Arctic-64 <77393546+Arctic-64@users.noreply.github.com>
2022-03-21 03:00:57 +0000

    meteorite_clean_data_finished

---
+++ Meteorites_PDA/Cleaning_Meteorites,R.R
@@ -0,0 +1,26 @@
+library(janitor)
+library(tidyverse)
+library(assertr)
+library(testthat)
+
+meteorites = read.csv("meteorite_landings.csv")
+meteorites = janitor::clean_names(meteorites)
+
+meteorites = mutate(meteorites, geo_location = str_sub(geo_location, 2, -2))
+meteorites = separate(meteorites, geo_location, into = c("lattitude", "longditude"), sep = ", ")
+
+meteorites = mutate(meteorites, lattitude = replace_na(lattitude, "0"))
+meteorites = mutate(meteorites, longditude = replace_na(longditude, "0"))
+
+meteorites = filter(meteorites, mass_g > 1000)
+
+meteorites = arrange(meteorites, year)
+
+meteorites = mutate(meteorites, lattitude = as.numeric(lattitude))
+meteorites = mutate(meteorites, longditude = as.numeric(longditude))
+
+verify(meteorites, lattitude > -90.0 & lattitude < 90.0 & longditude > -180.0 & longditude < 180.0)
+verify(meteorites, has_all_names("id", "name", "mass_g", "fall", "year", "lattitude", "longditude"))
+print("TESTS PASSED")
+
+write.csv(meteorites, "meteorite_landings_CLEAN.csv")
